88b857dd03d1eaf567e9c5c0a6b3de1045087710
muhamed <muhamedkorac0@gmail.com>
2019-02-15 19:07:49 +0100

    Ispravljena greska u programu.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('ssss')
+
+cene_proizvoda = [560, 130, 420, 415.5, 65.99, 78.99, 325.55]
+sortirana_lista = sorted(cene_proizvoda)
+print(sortirana_lista)
+tri_najjeftinija = sortirana_lista[0:3]
+tri_najskuplja = sortirana_lista[-3:]
+print('Tri najskuplja su : ', tri_najskuplja,
+      ' Tri najjeftinija su: ', tri_najjeftinija)
